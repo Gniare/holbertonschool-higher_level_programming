5580d128f2d8d05ced57861400f6633c6d7c0998
Gniare <niare09@hotmail.fr>
2024-05-23 15:41:40 +0200

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-    for ii in range(i + 1, 10):
-        print("{:d}{:d}".format(i, ii), end=", ")
-print("{:d}{:d}".format(i + 1, ii))
+for number in range(0, 99):
+    print("{:02d}".format(number), end=(', '))
+print("{}".format(number+1))
